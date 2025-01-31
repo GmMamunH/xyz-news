fae1c47f5fde454aba164983ab338760a0b917fb
GmMamunH <gmmamun3004@gmail.com>
2025-01-31 22:31:08 +0600

    fixed(home): fixed type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
-async function getNews() {
+type NewsItem = {
+  id: string; // অথবা number, যদি ID সংখ্যা হয়
+  title: string;
+  category: string;
+};
+
+async function getNews(): Promise<NewsItem[]> {
   const res = await fetch("https://news-api-seven-ivory.vercel.app/api/news");
   return res.json();
 }
+
 export default async function HomePage() {
-  const news = await getNews();
+  const news: NewsItem[] = await getNews();
+
   return (
-    <>
-      <div>
-        <h1>Latest News</h1>
-        <ul>
-          {news.map((item: any) => (
-            <li key={item.id}>
-              {item.title} - {item.category}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <div>
+      <h1>Latest News</h1>
+      <ul>
+        {news.map((item: NewsItem) => (
+          <li key={item.id}>
+            {item.title} - {item.category}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
